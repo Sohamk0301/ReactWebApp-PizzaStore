02be927ceee37075436ba3fee6db0a87844c1d80
Vachana <sholapurvachana@gmail.com>
2023-03-23 20:35:57 +0530

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+import './App.css';
+
+import Home from './screens/Home';
+
+
+class App extends Component {
+  render() {
+    return (
+     
+      <div>
+      <Home></Home>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
